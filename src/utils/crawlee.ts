402f5724bfcc90b744a294089aa7e0c88f3276ae
Serkan YESILDAG <syesildag@hotmail.com>
2025-03-29 21:26:58 +0100

    crawlee: fix

--- src/utils/crawlee.ts
+++ src/utils/crawlee.ts
@@ -24,15 +24,17 @@ const crawler = new PlaywrightCrawler({
       log.info(`Processing ${request.url}...`);
 
       // A function to be evaluated by Playwright within the browser context.
-      const data = await page.$$eval('.athing', ($posts) => {
-         const scrapedData: { title: string; rank: string; href: string }[] = [];
+      const data = await page.$$eval('div.body-post', ($posts) => {
+         const scrapedData: { title: string, content: string, tags: string, date: string, href: string }[] = [];
 
          // We're getting the title, rank and URL of each post on Hacker News.
          $posts.forEach(($post) => {
             scrapedData.push({
-               title: ($post.querySelector('.title a') as HTMLElement)?.innerText || '',
-               rank: ($post.querySelector('.rank') as HTMLElement)?.innerText || '',
-               href: ($post.querySelector('.title a') as HTMLAnchorElement)?.href || '',
+               title: ($post.querySelector('h2.home-title') as HTMLElement)?.innerText || '',
+               content: ($post.querySelector('div.home-desc') as HTMLElement)?.innerText || '',
+               tags: ($post.querySelector('.h-tags') as HTMLElement)?.innerText || '',
+               date: ($post.querySelector('.h-datetime') as HTMLElement)?.childNodes[1].textContent || '',
+               href: ($post.querySelector('a.story-link') as HTMLAnchorElement)?.href || '',
             });
          });
 
@@ -42,12 +44,18 @@ const crawler = new PlaywrightCrawler({
       // Store the results to the default dataset.
       await pushData(data);
 
-      // Find a link to the next page and enqueue it if it exists.
-      const infos = await enqueueLinks({
-         selector: '.morelink',
-      });
-
-      if (infos.processedRequests.length === 0) log.info(`${request.url} is the last page!`);
+      if(data.length > 0) {
+         const minDate = new Date(Math.min(...data.map(item => new Date(item.date)) as any));
+         let lastWeek = new Date();
+         lastWeek.setDate(lastWeek.getDate() - 7);
+         if (minDate > lastWeek) {
+            // Find a link to the next page and enqueue it if it exists.
+            const infos = await enqueueLinks({
+               selector: '.blog-pager-older-link-mobile',
+            });
+            if (infos.processedRequests.length === 0) log.info(`${request.url} is the last page!`);
+         }
+      }
    },
 
    // This function is called if the page processing failed more than maxRequestRetries+1 times.
@@ -57,7 +65,7 @@ const crawler = new PlaywrightCrawler({
 });
 
 (async () => {
-   await crawler.addRequests(['https://news.ycombinator.com/']);
+   await crawler.addRequests(['https://thehackernews.com/']);
 
    // Run the crawler and wait for it to finish.
    await crawler.run();
