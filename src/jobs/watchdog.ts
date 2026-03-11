91ddc166d45ac28edb6f6f942fa67347f69d692e
Serkan YESILDAG <syesildag@hotmail.com>
2026-03-12 00:00:16 +0100

    feat: implement dynamic job scheduling and management with loadDynamicJobs function

--- src/jobs/watchdog.ts
+++ src/jobs/watchdog.ts
@@ -1,23 +1,22 @@
 import { Range, RecurrenceRule } from "node-schedule";
 import JobFactory from "../utils/jobFactory";
 import Logger from "../utils/logger";
+import { loadDynamicJobs } from "../utils/dynamicJobLoader";
 
 export default class Watchdog extends JobFactory {
 
-   constructor() {
-      super();
-      this.setEnable(false);
-   }
-
    protected getSpec() {
       const rule = new RecurrenceRule();
-      rule.minute = new Range(0, 59, 1);
+      rule.minute = new Range(0, 59, 5);
       return rule;
    }
 
    protected getJobCallback() {
       return (fireDate: Date) => {
          Logger.debug("watchDog: " + fireDate);
+         loadDynamicJobs().catch(err =>
+            Logger.error(`[Watchdog] loadDynamicJobs error: ${err}`)
+         );
       }
    }
 }
